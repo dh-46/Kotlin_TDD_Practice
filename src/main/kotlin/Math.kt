261a3c8e0e6264c83073e61fb50a2a9966ffea6f
danielhuang-emct <daniel.h@emct.com.tw>
2020-04-09 09:42:19 +0800

    2020/04/09 - 修改邏輯讓測試失敗
    - 看看測試失敗的結果長得怎樣
    - 注意測試程式的正確性，如果寫錯就失去測試的意義。

--- src/main/kotlin/Math.kt
+++ src/main/kotlin/Math.kt
@@ -3,6 +3,7 @@
  */
 class Math {
     fun add(number1: Int, number2: Int): Int {
-         return number1 + number2
+         // return number1 + number2
+        return number1 - number2 // 刻意製造錯誤的邏輯
     }
 }
--- src/test/kotlin/MathTest.kt
+++ src/test/kotlin/MathTest.kt
@@ -11,5 +11,8 @@ class MathTest {
         val expected = 3 // 預期結果
         val actualNumber = Math().add(1,2) // 呼叫被測試的方法
         Assert.assertEquals(expected, actualNumber) // 驗證預期與結果
+        // 作者表示:
+        // 最好的撰寫方法，是先寫一個必定會失敗的測試，
+        // 來驗證測試程式碼有無錯誤，避免被測試的程式怎麼寫，測試結果都是Passed。
     }
 }
